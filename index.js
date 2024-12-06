9f38703e646ff2fa67937a3f4c24035c43af1e16
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-12-06 08:36:29 +0000

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -25,6 +25,34 @@ function createShortURL(url) {
   return shorturl;
 }
 
+function findOriginalURL(urlArray, shorturl){
+  for(let i = 0; i < urlArray.length; i++){
+    if(urlArray[i].shortURL === shorturl){
+      return urlArray[i]
+    }
+  }
+  return {error: "invalid url"}
+}
+
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  console.log(shortURLs)
+  next(); // Pass control to the next middleware or route handler
+});
+
+
+app.get("/api/shorturl/:urlNo", (req, res) => {
+  let urlNo = req.params.urlNo
+  urlNo = Number(urlNo) 
+  const urlEntry = (findOriginalURL(shortURLs, urlNo))
+  if(urlEntry.error){
+    return res.status(404).json(urlEntry);
+  }
+
+  res.redirect(urlEntry.original_url); 
+})
+
 app.post("/api/shorturl", function (req, res) {
   //Regex for determining URL validity
   const urlRegex =
@@ -36,18 +64,16 @@ app.post("/api/shorturl", function (req, res) {
   //Throw error if url is invalid
   if (!urlRegex.test(url)) {
     return res.json({ error: "invalid url" });
-  } 
+  }
 
-  try {
-    //check if url actually exists
-    const urlObj = new URL(url);
-    const hostname = urlObj.hostname;
+  //check if url actually exists
+  const urlObj = new URL(url);
+  const hostname = urlObj.hostname;
 
-    dns.lookup(hostname, (err, addresses, family) => {
-      if (err) {
-        return res.status(400).json({ error: "invalid url" });
-      }
-    });
+  dns.lookup(hostname, (err, addresses, family) => {
+    if (err) {
+      return res.status(400).json({ error: "invalid url" });
+    }
 
     //Check if url already exists in url array
     const existingEntry = shortURLs.find((entry) => entry.original_url == url);
@@ -60,14 +86,12 @@ app.post("/api/shorturl", function (req, res) {
       });
     }
 
-    //debug
-    console.log(shortURLs);
-
     //If no existing entry, generate new shorturl
     res.json({ url: url, shorturl: createShortURL(url) });
-  } catch (error) {
-    return res.status(400).json({ error: 'invalid url' });
-  }
+  });
+
+  //debug
+  console.log(shortURLs);
 });
 
 app.listen(port, function () {
