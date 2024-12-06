5dfafd3d607811d1caab7016bb256bd6ec8f61ee
OgagaAkpowenre1 Daniel <ogagaogheneakpowenre@gmail.com>
2024-12-06 09:20:15 +0000

    Changes to be committed: 	modified:   index.js 	renamed:    views/index.html -> public/index.html 	new file:   vercel.json

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+  res.sendFile(process.cwd() + "/public/index.html");
 });
 
 const shortURLs = [];
@@ -25,33 +25,32 @@ function createShortURL(url) {
   return shorturl;
 }
 
-function findOriginalURL(urlArray, shorturl){
-  for(let i = 0; i < urlArray.length; i++){
-    if(urlArray[i].shortURL === shorturl){
-      return urlArray[i]
+function findOriginalURL(urlArray, shorturl) {
+  for (let i = 0; i < urlArray.length; i++) {
+    if (urlArray[i].shortURL === shorturl) {
+      return urlArray[i];
     }
   }
-  return {error: "invalid url"}
+  return { error: "invalid url" };
 }
 
 // Logging middleware
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
-  console.log(shortURLs)
+  console.log(shortURLs);
   next(); // Pass control to the next middleware or route handler
 });
 
-
 app.get("/api/shorturl/:urlNo", (req, res) => {
-  let urlNo = req.params.urlNo
-  urlNo = Number(urlNo) 
-  const urlEntry = (findOriginalURL(shortURLs, urlNo))
-  if(urlEntry.error){
+  let urlNo = req.params.urlNo;
+  urlNo = Number(urlNo);
+  const urlEntry = findOriginalURL(shortURLs, urlNo);
+  if (urlEntry.error) {
     return res.status(404).json(urlEntry);
   }
 
-  res.redirect(urlEntry.original_url); 
-})
+  res.redirect(urlEntry.original_url);
+});
 
 app.post("/api/shorturl", function (req, res) {
   //Regex for determining URL validity
@@ -71,24 +70,24 @@ app.post("/api/shorturl", function (req, res) {
   const hostname = urlObj.hostname;
 
   dns.lookup(hostname, (err, addresses, family) => {
-    if (err) {
-      return res.status(400).json({ error: "invalid url" });
-    }
-
-    //Check if url already exists in url array
-    const existingEntry = shortURLs.find((entry) => entry.original_url == url);
-    if (existingEntry) {
-      //debug
-      console.log(shortURLs);
-      return res.json({
-        original_url: existingEntry.original_url,
-        shorturl: existingEntry.shortURL,
-      });
-    }
-
-    //If no existing entry, generate new shorturl
-    res.json({ url: url, shorturl: createShortURL(url) });
+    // if (err) {
+    //   return res.status(400).json({ error: "invalid url" });
+    // }
   });
+
+  //Check if url already exists in url array
+  const existingEntry = shortURLs.find((entry) => entry.original_url == url);
+  if (existingEntry) {
+    //debug
+    console.log(shortURLs);
+    return res.json({
+      "original_url": existingEntry.original_url,
+      "short_url": existingEntry.shortURL,
+    });
+  }
+
+  //If no existing entry, generate new shorturl
+  res.json({ "original_url": url, "short_url": createShortURL(url) });
 
   //debug
   console.log(shortURLs);
