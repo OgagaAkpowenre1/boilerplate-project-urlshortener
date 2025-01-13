f798dd1acda9b6e625d8afb627db72fadebda74e
ogagaogheneakpowenre <ogagaogheneakpowenre@gmail.com>
2025-01-13 06:12:13 +0000

    modified:   index.js 	modified:   vercel.json

--- index.js
+++ index.js
@@ -93,6 +93,8 @@ app.post("/api/shorturl", function (req, res) {
   console.log(shortURLs);
 });
 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+// app.listen(port, function () {
+//   console.log(`Listening on port ${port}`);
+// });
+
+module.exports = app;
